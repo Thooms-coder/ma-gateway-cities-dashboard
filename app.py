0ec3564991f98f17fcb5169c21f852c96126678a
Thooms-coder <mutsamungoshi@gmail.com>
2026-02-22 01:16:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -27,12 +27,14 @@ if city_fips:
 from sqlalchemy import text
 from src.db import engine
 
+st.subheader("Available Tables")
+
 with engine.connect() as conn:
     result = conn.execute(text("""
-        SELECT table_name 
+        SELECT table_name
         FROM information_schema.tables
         WHERE table_schema = 'public';
     """))
     tables = [row[0] for row in result]
 
-print(tables)
+st.write(tables)
