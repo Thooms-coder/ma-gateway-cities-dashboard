4c0a1d06944a271ba659c7d39d7e36b921b30600
Thooms-coder <mutsamungoshi@gmail.com>
2026-02-22 09:21:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -447,10 +447,11 @@ with st.container():
         latest_year = df_primary["year"].max()
     else:
         latest_year = None
-
+        
+    st.write("Primary FIPS:", primary_fips)
+    st.write("Latest Year from FB table:", latest_year)
+    
     if latest_year is not None:
-        st.write("Primary FIPS:", primary_fips)
-        st.write("Latest Year from FB table:", latest_year)
         df_origins = get_foreign_born_by_country(primary_fips, latest_year)
 
         if not df_origins.empty:
