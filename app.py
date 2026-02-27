c280067eb2354c70616fbab6cebc023293889181
Thooms-coder <mutsamungoshi@gmail.com>
2026-02-27 01:51:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -457,7 +457,7 @@ with tab_map:
             "CHELSEA": (0, -0.06),
             "FITCHBURG": (0.010, -0.010),
             "REVERE": (0.010, 0.010),
-            "EVERETT": (-0.010, -0.010),
+            "EVERETT": (-0.020, -0.010),
         }
 
         for town_name in locations:
