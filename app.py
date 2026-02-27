256c1918aaae0629e2d4da1f536a4a399ada4d65
Thooms-coder <mutsamungoshi@gmail.com>
2026-02-27 01:43:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -497,7 +497,7 @@ with tab_map:
                 mode="text",
                 text=label_text,
                 textfont=dict(
-                    size=9,
+                    size=11,
                     color="#111827"
                 ),
                 textposition="middle center",
