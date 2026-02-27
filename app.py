08f976b2598dbe51108fde4a8605783c68d9377c
Thooms-coder <mutsamungoshi@gmail.com>
2026-02-27 01:33:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -454,7 +454,7 @@ with tab_map:
             "LEOMINSTER": (-0.020, 0.020),
             "METHUEN": (0.012, 0.012),
             "MALDEN": (0.010, -0.010),
-            "CHELSEA": (-0.012, 0.010),
+            "CHELSEA": (-0.020, 0.012),
         }
 
         for town_name in locations:
